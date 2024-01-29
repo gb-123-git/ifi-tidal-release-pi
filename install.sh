8083600b0c7927dec198b25cc995e1a1a1316c8a
gb-123-git <64229388+gb-123-git@users.noreply.github.com>
2024-01-29 20:05:45 +0000

    minor changes to install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,8 @@ apt install -y ./required_libs_fixedVer/libcurl3_7.52.1-5+deb9u16_armhf --allow-
 chmod +x $INSTALL_DIR/play
 chmod +x $INSTALL_DIR/bin/*
 chmod +x $INSTALL_DIR/pa_devs/run.sh
+chmod +x $INSTALL_DIR/list-devices.sh
+chmod +x $INSTALL_DIR/re_deploy-service
 
 # deploy files
 cp $INSTALL_DIR/ifi-streamer-tidal-connect.service /lib/systemd/system/
